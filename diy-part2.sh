88029c1c2177b98585ea4d97b3f628d5adc98a1e
shuai1058 <105528871+shuai1058@users.noreply.github.com>
2022-05-22 23:35:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,16 +20,12 @@
 
 
 ##补充汉化       
-cp -f ./feeds/liwenwu1058/files/udpxy.lua ./feeds/luci/applications/luci-app-udpxy/luasrc/model/cbi
-cp -f ./feeds/liwenwu1058/files/mwan3.po ./feeds/luci/applications/luci-app-mwan3/po/zh-cn
+cp -f ./feeds/shuai1058/files/udpxy.lua ./feeds/luci/applications/luci-app-udpxy/luasrc/model/cbi
+cp -f ./feeds/shuai1058/files/mwan3.po ./feeds/luci/applications/luci-app-mwan3/po/zh-cn
               
 ##配置ip等
 sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm-k3|TARGET_DEVICES += phicomm-k3| ; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
-# sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
-
-##替换K3无线驱动为69027
-# rm -rf ./package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
-# svn export https://github.com/xiangfeidexiaohuo/Phicomm-K3_Wireless-Firmware/trunk/brcmfmac4366c-pcie.bin_69027 ./package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 
 ##取消bootstrap为默认主题
@@ -37,12 +33,6 @@ sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' feeds/luci/collections/luci-nginx/Makefile
 
-echo '移除bcm53xx中的其他机型'
-sed -i '421,453d' target/linux/bcm53xx/image/Makefile
-sed -i '140,412d' target/linux/bcm53xx/image/Makefile
-sed -i 's/$(USB3_PACKAGES) k3screenctrl/luci-app-k3screenctrl/g' target/linux/bcm53xx/image/Makefile
-# sed -n '140,146p' target/linux/bcm53xx/image/Makefile
-echo '=========Remove other devices of bcm53xx OK!========='
 
 
 
@@ -57,35 +47,6 @@ echo '=========Alert upnp binding file directory!========='
 ##更改主机名
 sed -i "s/hostname='.*'/hostname='Phicomm-K3'/g" package/base-files/files/bin/config_generate
 
-##FQ全部调到VPN菜单
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-ssr-plus/luasrc/controller/*.lua
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/*.lua
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-ssr-plus/luasrc/view/shadowsocksr/*.htm
-
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-passwall/luasrc/controller/*.lua
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-passwall/luasrc/model/cbi/passwall/api/*.lua
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-passwall/luasrc/model/cbi/passwall/client/*.lua
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-passwall/luasrc/model/cbi/passwall/server/*.lua
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-passwall/luasrc/view/passwall/app_update/*.htm
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-passwall/luasrc/view/passwall/global/*.htm
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-passwall/luasrc/view/passwall/haproxy/*.htm
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-passwall/luasrc/view/passwall/log/*.htm
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-passwall/luasrc/view/passwall/node_list/*.htm
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-passwall/luasrc/view/passwall/rule/*.htm
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-passwall/luasrc/view/passwall/server/*.htm
-
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-vssr/luasrc/controller/*.lua
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-vssr/luasrc/model/cbi/vssr/*.lua
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-vssr/luasrc/view/vssr/*.htm
-
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-openclash/luasrc/controller/*.lua
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-openclash/luasrc/*.lua
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-openclash/luasrc/model/cbi/openclash/*.lua
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-openclash/luasrc/view/openclash/*.htm
-
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-bypass/luasrc/controller/*.lua
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-bypass/luasrc/model/cbi/bypass/*.lua
-# sed -i 's/services/vpn/g' package/feeds/liwenwu1058/luci-app-bypass/luasrc/view/bypass/*.htm
 
 
 sed -i '/option Interface/d'  package/network/services/dropbear/files/dropbear.config
